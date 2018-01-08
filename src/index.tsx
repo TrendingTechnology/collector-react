d90e09d6a0ec502368cd34f0f44c2ef7c49225cc
Benjamin Humphrey <humphreybc@gmail.com>
2018-01-08 15:56:05 +1100

    Fix undefined condition

--- src/index.tsx
+++ src/index.tsx
@@ -68,7 +68,7 @@ export class Collector extends React.Component<Props> {
           break;
         }
         case "send": {
-          this.props.onSend ? this.props.onSend() : null;
+          this.props.onSend != undefined ? this.props.onSend() : null;
           break;
         }
       }
