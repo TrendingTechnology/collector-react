b610564debf4ef0ee3e6342e8ecb68d038202970
Bradley Ayers <bradley.ayers@gmail.com>
2018-01-08 11:41:08 +1100

    Initial groundwork

---
+++ src/__spec__/index.spec.ts
@@ -0,0 +1,6 @@
+import "jest";
+import { Collector } from "../";
+
+it("is a function", () => {
+  expect(Collector).toBeInstanceOf(Function);
+});
---
+++ src/__stories__/index.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { Collector } from "../";
+
+storiesOf("Example", module)
+  .add("empty", () => <Collector />);
